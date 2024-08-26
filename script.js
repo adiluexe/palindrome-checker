052d5cf8256b6ab105dfac1913cf30c6217fa7ad
adiluexe <adiluexe@gmail.com>
2024-08-26 19:26:05 +0800

    Feat: Add checkPalindrome function

---
+++ script.js
@@ -0,0 +1,15 @@
+const checkButton = document.querySelector("#check-btn");
+const textInput = document.querySelector("#text-input");
+const result = document.querySelector("#result");
+
+checkButton.addEventListener("click", checkPalindrome);
+
+function checkPalindrome() {
+  const input = textInput.value.toLowerCase();
+  const reversed = input.split("").reverse().join("");
+  if (input === reversed) {
+    result.textContent = `${input} is a palindrome!`;
+  } else {
+    result.textContent = `${input} is not a palindrome!`;
+  }
+}
