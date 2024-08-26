2d9eaa747ece41c9a505d5be9e3548dcbd6c1028
adiluexe <adiluexe@gmail.com>
2024-08-26 19:33:13 +0800

    Feat: Check if input is empty or has whitespace included

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ const result = document.querySelector("#result");
 checkButton.addEventListener("click", checkPalindrome);
 
 function checkPalindrome() {
-  const input = textInput.value.toLowerCase();
+  const input = textInput.value.trim();
+  if (input === "") {
+    alert("Please input a value");
+    result.textContent = "Please enter a word or sentence to check";
+    return;
+  }
+
   const reversed = input.split("").reverse().join("");
   if (input === reversed) {
     result.textContent = `${input} is a palindrome!`;
